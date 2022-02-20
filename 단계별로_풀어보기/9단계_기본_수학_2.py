d48f9a8ab18e7bed925c8eaa23ad3f035c3b36fa
idanhae <idanhae@gmail.com>
2022-02-20 23:07:18 +0900

    #1085

--- 단계별로_풀어보기/9단계_기본_수학_2.py
+++ 단계별로_풀어보기/9단계_기본_수학_2.py
@@ -1,6 +1,20 @@
 # 단계별로 풀어보기
 ## 9단계 기본 수학 2
 
+#1085 직사각형에서 탈출
+x, y, w, h = map(int,input().split())
+hw = w/2
+hh = h/2
+
+if x<=hw and y<=hh:
+    print(min(x,y))
+elif x<=hw and y>=hh:
+    print(min(x,h-y))
+elif x>=hw and y<=hh:
+    print(min(w-x,y))
+elif x>=hw and y>=hh:
+    print(min(w-x,h-y))
+
 #4153 직각삼각형
 while True:
     A,B,C = map(int, input().split())
@@ -18,7 +32,6 @@ while True:
 
 #3053 택시 기하학
 import math
-
 R = int(input())
 print(math.pi*(R**2))
 print(2*(R**2)) # 택시 기하학의 원=내가 아는 마름모 인듯?!
